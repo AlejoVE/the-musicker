77df66e8d181ca207c04f50b6c4d240c0fd88c1f
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-06-11 17:02:42 +0200

    getAll method completed in controllers.js

---
+++ api/artists/controllers.js
@@ -0,0 +1,16 @@
+const db = require("../db-connection");
+
+const controllers = {
+  getAll: (req, res) => {
+    const sql = `SELECT * FROM artists`;
+
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+
+      res.json(rows);
+    });
+  },
+};
